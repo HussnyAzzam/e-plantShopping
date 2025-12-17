dcc5f4265f6cf2e447e701a933f76bd0a0860c18
HussnyAzzam <16493099+HussnyAzzam@users.noreply.github.com>
2025-12-16 23:59:16 -0500

    Checkout message to coming Soon

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -21,7 +21,7 @@ const CartItem = ({ onContinueShopping }) => {
 
   const handleCheckoutShopping = (e) => {
     e.preventDefault();
-    alert('Functionality to be added for future reference');
+    alert('Coming Soon');
   };
 
   const handleIncrement = (item) => {
